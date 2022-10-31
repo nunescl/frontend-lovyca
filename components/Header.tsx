c8154ee938f783d0433955d054d4a4fb88fb5295
Ana Mendes <anaclaranunes@gmail.com>
2022-10-31 16:08:37 -0300

    feat: create header-component

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="bg-gray-500 flex-row text-gray-600 text-xs py-1 px-1 pl-2">
+      <i className="fa-solid fa-circle px-0.5 py-1"></i>
+      <i className="fa-solid fa-circle px-0.5 py-1"></i>
+      <i className="fa-solid fa-circle px-0.5 py-1"></i>
+    </header>
+  );
+}
