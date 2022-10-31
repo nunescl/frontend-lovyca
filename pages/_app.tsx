6f7a7b6c595f6c2a1fc02a43e7e9a00dde94d6f3
Ana Mendes <anaclaranunes@gmail.com>
2022-10-31 16:12:23 -0300

    feat: add auth-context and layout on App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { AuthProvider } from '../context/AuthContext';
+import Layout from '../components/Layout';
+import UserDashboard from './user/search';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps): any {
+  return (
+    <AuthProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AuthProvider>
+  );
 }
