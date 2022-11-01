2912562b4a54b05d9bbc6b71b6297b9b5395d659
Ana Mendes <anaclaranunes@gmail.com>
2022-11-01 11:10:45 -0300

    feat: implement search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
-export default function SearchBar({ placeholder }: { placeholder?: string }) {
+export default function SearchBar({
+  placeholder,
+  search,
+  onChange,
+}: {
+  placeholder?: string;
+  search?: string;
+  onChange: any;
+}) {
   return (
     <div className="flex justify-bettween">
       <form action="" className="w-full">
@@ -8,6 +16,9 @@ export default function SearchBar({ placeholder }: { placeholder?: string }) {
           <input
             className="h-9 w-full p-2 duration-300 outline-none border border-1 border-solid border-gray-400 rounded focus:border-b-teal-500 text-slate-900 spin-button-none"
             placeholder={placeholder}
+            type="search"
+            value={search}
+            onChange={onChange}
           ></input>
           <i className="fa-solid fa-magnifying-glass absolute ml-3 pr-5 pointer-events-none"></i>
         </div>
--- pages/user/dashboard.tsx
+++ pages/user/dashboard.tsx
@@ -5,11 +5,30 @@ import Link from 'next/link';
 import Head from 'next/head';
 
 export default function UserDashboard() {
-  const [services, setServices] = useState<any>();
+  const [services, setServices] = useState<string[]>();
+  const [search, setSearch] = useState<string>();
 
   React.useEffect(() => {
     SERV_GET(setServices);
   }, []);
+
+  const handleSearch = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    let { value } = target;
+
+    if (value.length >= 3) {
+      setSearch(value);
+    }
+    if (value.length === 0) {
+      setSearch('');
+    }
+  };
+
+  const searchResult =
+    search && services
+      ? services.filter((services: any) =>
+          services.name.toLowerCase().includes(search.toLowerCase()),
+        )
+      : services;
 
   return (
     <>
@@ -26,7 +45,12 @@ export default function UserDashboard() {
           <p className="text-xs self-center mb-1">
             Encontre o seu serviço adicionado
           </p>
-          <SearchBar placeholder="Ex.: Hidráulica, Elétrica, Pintura e Decoração..." />
+          <SearchBar
+            placeholder="Ex.: Corte de grama, troca de lâmpada..."
+            onChange={({ target }: React.ChangeEvent<HTMLInputElement>) =>
+              handleSearch({ target })
+            }
+          />
         </div>
       </header>
 
@@ -41,26 +65,26 @@ export default function UserDashboard() {
               <th className="pl-2">Preço</th>
             </tr>
           </thead>
-          {services &&
-            services.map((service: any) => (
+          {searchResult &&
+            searchResult.map((service: any) => (
               <tbody key={service.id}>
                 <tr
-                  key={service.id}
+                  key={service.id + '1'}
                   className="bg-gray-100 border-b-t border-solid border-gray-400"
                 >
-                  <td key={service.id} className="pl-2 py-2">
+                  <td key={service.id + '2'} className="pl-2 py-2">
                     {service.name}
                   </td>
-                  <td key={service.id} className="pl-2 py-2">
+                  <td key={service.id + '3'} className="pl-2 py-2">
                     {service.category}
                   </td>
-                  <td key={service.id} className="pl-2 py-2">
+                  <td key={service.id + '4'} className="pl-2 py-2">
                     {service.created_at}
                   </td>
-                  <td key={service.id} className="pl-2 py-2">
+                  <td key={service.id + '5'} className="pl-2 py-2">
                     {service.status}
                   </td>
-                  <td key={service.id} className="pl-2 py-2">
+                  <td key={service.id + '6'} className="pl-2 py-2">
                     {service.price}
                   </td>
                 </tr>
